03f0280be3b70f83805d1cf33f999e3471275c1c
Zzy-07qi <2943914872@qq.com>
2026-04-19 12:11:09 +0800

    Delete Always Beside You directory

--- Always Beside You/src/api/gif.js
+++
@@ -1,9 +0,0 @@
-import request from "../utils/request";
-
-
-export const getGif = () => {
-    return request({
-        url: '/gifs',
-        method: 'GET'
-    })
-}
--- Always Beside You/src/api/music.js
+++
@@ -1,9 +0,0 @@
-import request from "../utils/request"
-
-
-export const getEnvirMusic = () => {
-    return request({
-        url: '/ambient-sounds',
-        method: 'GET'
-    })
-}
